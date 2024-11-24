9894b3dd9446e50daca9c03b48141bbddc6b60b0
Eduardo Leines <jorge.leines@epn.edu.ec>
2024-11-24 02:56:43 -0500

    Actualizacion de localizacion implementada, se muestra en consola
    La actualizacion  de prueba se ejecuta al iniciar el programa, se debe implementar redireccionamiento de rutas en el frontend para poder implementar de mejor manera cada funcionalidad

--- public/services/indexput.js
+++ public/services/indexput.js
@@ -1,2 +1,17 @@
 
-updateLocation()
+const locationId = 1; // ID de la ubicación a actualizar
+const updatedLocationData = {
+  name: "Sucursal Centro Actualizada",
+  address: "Avenida Central 456",
+  city: "Nueva Ciudad",
+  country: "Nuevo País",
+};
+
+// Llamar a la función para actualizar una ubicación
+updateLocation(locationId, updatedLocationData)
+  .then((response) => {
+    console.log("Respuesta del servidor:", response);
+  })
+  .catch((error) => {
+    console.error("Error al actualizar la ubicación:", error.message);
+  });
